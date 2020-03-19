01b1cbe10649d9c7825a43bf8abde19e9000770c
unknown <czegledi.nandor.lajos@gmail.com>
2020-03-19 14:18:36 +0100

    Add first version to github

--- app_asterisksPattern.js
+++ app_asterisksPattern.js
@@ -21,61 +21,60 @@ Make it harder by accepting a 2nd parameter for the position of the asterisks (l
 *******
 */
 function createAsterisksPattern(levels, position) {
-var patternLevels = [];
+    var patternLevels = [];
 
-function buildPattern(levels, position) {
-    for (var y = 0; y < levels; y++) {
-        // If array element isnt exist then create one to be able to expand later
-        if (!patternLevels[y]) patternLevels[y] = '';
+    function buildPattern(levels, position) {
+        for (var y = 0; y < levels; y++) {
+            // If array element isnt exist then create one to be able to expand later
+            if (!patternLevels[y]) patternLevels[y] = '';
 
-        for (var x = 0; x < levels - y; x++) {
-            patternLevels[y] = patternLevels[y] + '*';
+            for (var x = 0; x < levels - y; x++) {
+                patternLevels[y] = patternLevels[y] + '*';
 
-            // In centered position extend the pattern 
-            if (position === 'center' && y !== 0) {
-                patternLevels[y - 1] = patternLevels[y - 1] + '*';
+                // In centered position extend the pattern 
+                if (position === 'center' && y !== 0) {
+                    patternLevels[y - 1] = patternLevels[y - 1] + '*';
+                }
             }
         }
     }
-}
 
-// Adding spaces befor/after elements based on the positioning
-function positioningPattern(position) {
-    for (var i = 1; i < patternLevels.length; i++) {
-        switch (position) {
-            case 'left':
-                for (var y = 0; y < i; y++) {
-                    patternLevels[i] = patternLevels[i] + ' ';
-                }
-                break;
-            case 'right':
-                for (var y = 0; y < i; y++) {
-                    patternLevels[i] = ' ' + patternLevels[i];
-                }
-                break;
-            case 'center':
-                for (var y = 0; y < i; y++) {
-                    patternLevels[i] = ' ' + patternLevels[i] + ' ';
-                }
-                break;
+    // Adding spaces befor/after elements based on the positioning
+    function positioningPattern(position) {
+        for (var i = 1; i < patternLevels.length; i++) {
+            switch (position) {
+                case 'left':
+                    for (var y = 0; y < i; y++) {
+                        patternLevels[i] = patternLevels[i] + ' ';
+                    }
+                    break;
+                case 'right':
+                    for (var y = 0; y < i; y++) {
+                        patternLevels[i] = ' ' + patternLevels[i];
+                    }
+                    break;
+                case 'center':
+                    for (var y = 0; y < i; y++) {
+                        patternLevels[i] = ' ' + patternLevels[i] + ' ';
+                    }
+                    break;
+            }
         }
     }
-}
 
-// Print pattern into the console
-function printPattern(patternLevels) {
-    for (var i = patternLevels.length; i > 0; i--) {
-        console.log(patternLevels[i - 1]);
+    // Print pattern into the console
+    function printPattern() {
+        for (var i = patternLevels.length; i > 0; i--) {
+            console.log(patternLevels[i - 1]);
+        }
+
+        console.log(patternLevels);
     }
 
-    console.log(patternLevels);
-}
-    
     buildPattern(levels, position);
     positioningPattern(position);
-    printPattern(patternLevels);
+    printPattern();
 }
-
 // Start application
 createAsterisksPattern(4, 'left');
 createAsterisksPattern(4, 'right');
